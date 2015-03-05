d850e201416416dd24754a30311a0695146cc7e8
Geoffrey Plitt <geoff@gweb.org>
2015-03-05 15:17:00 -0800

    revised tests

--- test.js
+++ test.js
@@ -6,18 +6,28 @@ var memory_tests = require('./memory_tests');
 var requester = require('./requester')
 
 var TEST_DURATION_SECONDS = 20;
-var SCHEME = 'https';
+var CONCURRENCY = 16;
+
+var scheme = 'https'; // http
+
+console.log('NODE VERSION:', process.version);
+console.log('Scheme:', scheme);
 
 describe('stress tests', function() {
   it('can run rapid simulation for a while without errors or memory leaks', function(done) {
-    this.timeout((TEST_DURATION_SECONDS+1)*1000);
+    this.timeout((TEST_DURATION_SECONDS+5)*1000);
 
     var mem_before = null;
 
+    var stop = false;
     function loop() {
-      requester('GET', SCHEME, 'google.com', '/', {}, loop);
+      if(stop) return;
+      var path = '';
+      requester('GET', scheme, 'google.com', '/', {}, loop);
     }
-    loop();
+    for(var i=0; i<CONCURRENCY; i++) {
+      loop();
+    }
 
     // halfway through, store initial values
     setTimeout(function() {
@@ -26,8 +36,11 @@ describe('stress tests', function() {
 
     // at end, do final calculations
     setTimeout(function() {
-      memory_tests.memory_leak_end(mem_before);
-      done();
+      stop = true;
+      setTimeout(function() {
+        memory_tests.memory_leak_end(mem_before);
+        done();
+      }, 2*1000);
     }, TEST_DURATION_SECONDS*1000)
   });
 });
